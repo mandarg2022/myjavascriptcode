bc90a30c3601d133dfd22a2e5113862ea2c22d6c
mandarg2022 <113110642+mandarg2022@users.noreply.github.com>
2022-09-09 12:15:14 +0530

    Add files via upload

---
+++ javascript/filter.js
@@ -0,0 +1,5 @@
+const my_arr = [1, 2, 3, 4, 6];
+const double = my_arr.map((item) => item * 2);
+console.log(double);
+const evens = my_arr.filter((item) => item % 2 === 0);
+console.log(evens);
---
+++ javascript/ifelse.js
@@ -0,0 +1,6 @@
+age = 19;
+if (age > 18) {
+  console.log(`you cannot drive`);
+} else {
+  console.log(`you can drive`);
+}
